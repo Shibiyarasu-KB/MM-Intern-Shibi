5fb08f10ade1501c70df2d20efaf51443bba43a1
Shibiyarasu <shibiyarasukb@outlook.com>
2024-03-21 22:20:54 +0530

    js imp

---
+++ script.js
@@ -0,0 +1,8 @@
+
+//statements
+var shibi,raj=null;
+var Shibi="Techie the Lord"; //case sensitive , variables
+function dummy(){   //identifiers and function
+    console.log(typeof Shibi);
+}
+dummy();
