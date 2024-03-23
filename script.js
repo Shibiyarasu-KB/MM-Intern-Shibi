81f7f7fc86c42575e0740ea4f9920b07dca692bf
Shibiyarasu <shibiyarasukb@outlook.com>
2024-03-23 12:13:08 +0530

    milestone2 completed

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 
 //statements
-var shibi,raj=null;
+var shibi/*undefined variable*/,raj=null;
+
 var Shibi="Techie the Lord"; //case sensitive , variables
+
+var boo=true;
+
 function dummy(){   //identifiers and function
-    console.log(typeof Shibi);
+    if (boo==true){
+    console.log(typeof Shibi);}
+    ab=2;
+    console.log(ab);
 }
+
+var m={
+    name : 'Shibiyarasu K B',
+    age : 19 ,
+    city : 'Dpm'
+};
+
+
+//arrays
+var arr=[1,2,3,4,5];
+var ar={name:'shibi',age:18};
+
 dummy();
