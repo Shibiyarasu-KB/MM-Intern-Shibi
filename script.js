28a6199bf03e685c2c6c947a4cdf571bab1fc51d
Shibiyarasu <shibiyarasukb@outlook.com>
2024-03-26 22:44:30 +0530

    operators(2)

--- script.js
+++ script.js
@@ -25,3 +25,86 @@ var arr=[1,2,3,4,5];
 var ar={name:'shibi',age:18};
 
 dummy();
+
+
+
+//Operators
+
+//Unary Operators
+
+let k=6
+console.log(1+k);
+console.log(k-1);
+console.log(++k);
+console.log(++k);
+console.log(--k);
+console.log(k--);
+
+//Bitwise operators
+
+let s=5
+
+console.log(s&k);
+console.log(s | k);
+console.log(s^k);
+console.log(s<<1);
+console.log(s>>1);
+console.log(s>>>1);
+
+
+//Boolean operators
+
+let x=true,y=false;
+console.log(x&&y);
+console.log(x||y);
+console.log(!x);
+
+
+//Multiplicative operators
+
+let a=10,b=3;
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+
+//Additive operators
+
+console.log(a+b);
+console.log(a-b);
+
+
+//Relational operators
+
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>=b);
+
+//Equality operators
+
+console.log(x==y);
+console.log(x!=y);
+console.log(x===y);
+console.log(x!==y);
+
+
+//Conditional operators
+
+let m = (a >= 18) ? 'You are an adult' : 'You are a minor';
+
+
+//Assignment operators
+
+a+=1;
+a-=1;
+a/=1;
+a*=1;
+a%=1;
+
+
+//Comma operators
+
+let ab=(5,10);
+console.log(ab);
+
