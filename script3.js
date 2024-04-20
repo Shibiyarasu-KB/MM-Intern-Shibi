4fd46061e4fc46fd6fd2393b0c9b62d14d207068
Shibiyarasu-KB <shibiyarasukb@outlook.com>
2024-04-20 22:35:20 +0530

    lcm

--- script3.js
+++ script3.js
@@ -1,9 +1,19 @@
-let numbers = [3, 5, 7];
+let numbers = [3, 5, 7]; ///list of numbers
+
+// Math is object available in js , which provides mathematical functions 
+
+// ...variable_name is used to split the list of numbers or items
+
 let max = Math.max(...numbers);
 let lcm;
 
 while (true) {
     if (numbers.every(num => max % num === 0)) {
+
+        // numbers.every means , the each number in the list 
+
+        //num => max %num === 0 is the condition for the if statement , which is checked for all the numbers
+        
         lcm = max;
         break;
     }
